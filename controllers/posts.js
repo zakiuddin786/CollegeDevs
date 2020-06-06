b9e7afc0bb7a532c7a1c4cb8b0de1d31967ef466
Zaki udding <zakiuddin321@gmail.com>
2020-06-07 01:19:04 +0530

    added post route

---
+++ controllers/posts.js
@@ -0,0 +1,35 @@
+const { validationResult } = require("express-validator");
+
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Profile = require("../models/Profile");
+
+exports.addPost = async (req,res)=>{
+    const errors= validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array()
+            });
+        }
+
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        const newPost = new Post({
+            text : req.body.text,
+            name: user.name,
+            avatar:user.avatar,
+            user:req.user.id
+        })
+
+        const post = await newPost.save();
+        return res.json(post);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send({
+            msg:"Internal Server Error!"
+        })
+    }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
-router.get("/",(req,res)=>{
-    res.send("Post Route");
-})
+const auth = require("../middleware/auth");
+const postController = require("../controllers/posts")
+
+router.post("/",[auth,[
+    check("text","Text is required!").not().isEmpty()
+]],
+postController.addPost
+)
 
 module.exports = router;
